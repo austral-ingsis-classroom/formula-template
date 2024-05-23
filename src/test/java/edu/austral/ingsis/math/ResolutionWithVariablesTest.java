cabc751042165ec1b0ba214a86c43a891e423873
Tomás <troldanborjas@mail.austral.edu.ar>
2024-05-22 22:36:21 -0300

    Replaced code at ResolutionWithVariablesTest.java

--- src/test/java/edu/austral/ingsis/math/ResolutionWithVariablesTest.java
+++ src/test/java/edu/austral/ingsis/math/ResolutionWithVariablesTest.java
@@ -5,12 +5,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ResolutionWithVariablesTest {
 
   /** Case 1 + x where x = 3 */
   @Test
   public void shouldResolveFunction1() {
-    final Double result = 4d;
+    final Function function = new FunctionImpl(
+            new Sum(
+                    new Constant(1),
+                    new Variable("x")
+            )
+    );
+    final Double result = function.computeWithValues(Map.of("x", 3d));
 
     assertThat(result, equalTo(4d));
   }
@@ -18,7 +27,13 @@ public class ResolutionWithVariablesTest {
   /** Case 12 / div where div = 4 */
   @Test
   public void shouldResolveFunction2() {
-    final Double result = 3d;
+    final Function function = new FunctionImpl(
+            new Division(
+                    new Constant(12),
+                    new Variable("div")
+            )
+    );
+    final Double result = function.computeWithValues(Map.of("div", 4d));
 
     assertThat(result, equalTo(3d));
   }
@@ -26,7 +41,16 @@ public class ResolutionWithVariablesTest {
   /** Case (9 / x) * y where x = 3 and y = 4 */
   @Test
   public void shouldResolveFunction3() {
-    final Double result = 12d;
+    final Function function = new FunctionImpl(
+            new Product(
+                    new Division(
+                            new Constant(9),
+                            new Variable("x")
+                    ),
+                    new Variable("y")
+            )
+    );
+    final Double result = function.computeWithValues(Map.of("x", 3d, "y", 4d));
 
     assertThat(result, equalTo(12d));
   }
@@ -34,7 +58,16 @@ public class ResolutionWithVariablesTest {
   /** Case (27 / a) ^ b where a = 9 and b = 3 */
   @Test
   public void shouldResolveFunction4() {
-    final Double result = 27d;
+    final Function function = new FunctionImpl(
+            new Power(
+                    new Division(
+                            new Constant(27),
+                            new Variable("a")
+                    ),
+                    new Variable("b")
+            )
+    );
+    final Double result = function.computeWithValues(Map.of("a", 9d, "b", 3d));
 
     assertThat(result, equalTo(27d));
   }
@@ -42,7 +75,10 @@ public class ResolutionWithVariablesTest {
   /** Case z ^ (1/2) where z = 36 */
   @Test
   public void shouldResolveFunction5() {
-    final Double result = 6d;
+    final Function function = new FunctionImpl(
+      new Sqrt(new Variable("z"))
+    );
+    final Double result = function.computeWithValues(Map.of("z", 36d));
 
     assertThat(result, equalTo(6d));
   }
@@ -50,7 +86,13 @@ public class ResolutionWithVariablesTest {
   /** Case |value| - 8 where value = 8 */
   @Test
   public void shouldResolveFunction6() {
-    final Double result = 0d;
+    final Function function = new FunctionImpl(
+      new Difference(
+              new Module(new Variable("value")),
+              new Constant(8)
+      )
+    );
+    final Double result = function.computeWithValues(Map.of("value", 8d));
 
     assertThat(result, equalTo(0d));
   }
@@ -58,7 +100,13 @@ public class ResolutionWithVariablesTest {
   /** Case |value| - 8 where value = 8 */
   @Test
   public void shouldResolveFunction7() {
-    final Double result = 0d;
+    final Function function = new FunctionImpl(
+            new Difference(
+                    new Module(new Variable("value")),
+                    new Constant(8)
+            )
+    );
+    final Double result = function.computeWithValues(Map.of("value", 8d));
 
     assertThat(result, equalTo(0d));
   }
@@ -66,7 +114,16 @@ public class ResolutionWithVariablesTest {
   /** Case (5 - i) * 8 where i = 2 */
   @Test
   public void shouldResolveFunction8() {
-    final Double result = 24d;
+    final Function function = new FunctionImpl(
+            new Product(
+                    new Difference(
+                            new Constant(5),
+                            new Variable("i")
+                    ),
+                    new Constant(8)
+            )
+    );
+    final Double result = function.computeWithValues(Map.of("i", 2d));
 
     assertThat(result, equalTo(24d));
   }
